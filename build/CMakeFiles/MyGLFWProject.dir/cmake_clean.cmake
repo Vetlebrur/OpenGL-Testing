610527a38a75ce0744683764becee1c60e9cd7a9
Vetlebrur <vetlerod@gmail.com>
2024-06-15 18:48:45 +0200

    sgshjk

---
+++ build/CMakeFiles/MyGLFWProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyGLFWProject.dir/main.cpp.o"
+  "CMakeFiles/MyGLFWProject.dir/main.cpp.o.d"
+  "MyGLFWProject"
+  "MyGLFWProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyGLFWProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
